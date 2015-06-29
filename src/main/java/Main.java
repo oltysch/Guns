cf2dfd2ef5c036d753b0755bcbcb47a6d12cd0d4
oltysch <oleg_tyschenko777@mail.ru>
2015-06-29 18:31:45 +0600

    simple parser from lesson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,26 @@ import jdk.internal.org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
+import utils.SimpleSAXParser;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ResourceBundle;
 
 /**
  * Created by Admin on 28.06.2015.
  */
 public class Main {
-    public static void main(String[] args) throws SAXException, IOException {
-        XMLReader parser = XMLReaderFactory.createXMLReader();
-        org.xml.sax.InputSource input = new org.xml.sax.InputSource(Main.class.getClassLoader().getResourceAsStream("gun.xml"));
-        parser.parse(input);
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+//        test simple parser
+        javax.xml.parsers.SAXParserFactory spf = SAXParserFactory.newInstance();
+        spf.setValidating(false);
+        javax.xml.parsers.SAXParser sp = spf.newSAXParser();
+        SimpleSAXParser handler = new SimpleSAXParser();
+        InputStream inputStream = Main.class.getResourceAsStream("gun.xml");
+        sp.parse(inputStream, handler);
     }
 }
---
+++ src/main/java/utils/ListSAXParser.java
@@ -0,0 +1,7 @@
+package utils;
+
+/**
+ * Created by Admin on 29.06.2015.
+ */
+public class ListSAXParser {
+}
